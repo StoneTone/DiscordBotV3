e77a852b90a103bbb72a1b7e531ee22f13f234f3
StoneTone <thetony3535@gmail.com>
2024-05-09 16:38:35 -0500

    update: added dockerfile and cleanup

--- src/main/java/com/bot/discordbotv3/cnfg/BotConfig.java
+++ src/main/java/com/bot/discordbotv3/cnfg/BotConfig.java
@@ -1,7 +1,6 @@
 package com.bot.discordbotv3.cnfg;
 
 import com.bot.discordbotv3.listener.Listeners;
-import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
