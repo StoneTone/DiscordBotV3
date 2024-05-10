fa9d94eef8ad861ad1c1a226363121c5794083a8
StoneTone <thetony3535@gmail.com>
2024-05-10 15:59:51 -0500

    update: added dockerfile and cleanup

--- src/main/java/com/bot/discordbotv3/options/RoleOptions.java
+++ src/main/java/com/bot/discordbotv3/options/RoleOptions.java
@@ -6,12 +6,12 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 public class RoleOptions {
     public static OptionData handleRoleOptions(){
         return new OptionData(OptionType.STRING, "rolerequest", "Allows you to request a role", true)
-                .addChoice("Test", "912638897316589579")
-                .addChoice("Test", "1137982033671495761")
-                .addChoice("Test", "1044861143514099712")
-                .addChoice("Test", "695888137146335253")
-                .addChoice("Test", "698031938790752286")
-                .addChoice("Test", "572633970198446091")
-                .addChoice("Test", "747662211744399442");
+                .addChoice("ShreeSports", "912638897316589579")
+                .addChoice("ShreeActiveRoster", "1137982033671495761")
+                .addChoice("Shreeveloper", "1044861143514099712")
+                .addChoice("Amigos", "695888137146335253")
+                .addChoice("Geek Squad", "698031938790752286")
+                .addChoice("Branded Payment", "572633970198446091")
+                .addChoice("Peasant", "747662211744399442");
     }
 }
