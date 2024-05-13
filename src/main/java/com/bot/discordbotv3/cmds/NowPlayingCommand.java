1e861a8594632cc0275e5f5232c4192a7d2c3b39
StoneTone <thetony3535@gmail.com>
2024-05-13 11:41:43 -0500

    cache test

--- src/main/java/com/bot/discordbotv3/cmds/NowPlayingCommand.java
+++ src/main/java/com/bot/discordbotv3/cmds/NowPlayingCommand.java
@@ -14,7 +14,7 @@ public class NowPlayingCommand {
         GuildVoiceState memberVoiceState = member.getVoiceState();
 
         if(!memberVoiceState.inAudioChannel()) {
-            event.reply("You need to be in a voice channel").queue();
+            event.reply("You need to be in a voice channel").setEphemeral(true).queue();
             return;
         }
 
@@ -22,18 +22,18 @@ public class NowPlayingCommand {
         GuildVoiceState selfVoiceState = self.getVoiceState();
 
         if(!selfVoiceState.inAudioChannel()) {
-            event.reply("I am not in an audio channel").queue();
+            event.reply("I am not in an audio channel").setEphemeral(true).queue();
             return;
         }
 
         if(selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
-            event.reply("You are not in the same channel as me").queue();
+            event.reply("You are not in the same channel as me").setEphemeral(true).queue();
             return;
         }
 
         GuildMusicManager guildMusicManager = PlayerManager.get().getGuildMusicManager(event.getGuild());
         if(guildMusicManager.getTrackScheduler().getPlayer().getPlayingTrack() == null) {
-            event.reply("I am not playing anything").queue();
+            event.reply("I am not playing anything").setEphemeral(true).queue();
             return;
         }
         AudioTrackInfo info = guildMusicManager.getTrackScheduler().getPlayer().getPlayingTrack().getInfo();
--- src/main/java/com/bot/discordbotv3/cmds/SkipCommand.java
+++ src/main/java/com/bot/discordbotv3/cmds/SkipCommand.java
@@ -12,7 +12,7 @@ public class SkipCommand {
         GuildVoiceState memberVoiceState = member.getVoiceState();
 
         if(!memberVoiceState.inAudioChannel()) {
-            event.reply("You need to be in a voice channel").queue();
+            event.reply("You need to be in a voice channel").setEphemeral(true).queue();
             return;
         }
 
@@ -20,12 +20,12 @@ public class SkipCommand {
         GuildVoiceState selfVoiceState = self.getVoiceState();
 
         if(!selfVoiceState.inAudioChannel()) {
-            event.reply("I am not in an audio channel").queue();
+            event.reply("I am not in an audio channel").setEphemeral(true).queue();
             return;
         }
 
         if(selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
-            event.reply("You are not in the same channel as me").queue();
+            event.reply("You are not in the same channel as me").setEphemeral(true).queue();
             return;
         }
 
