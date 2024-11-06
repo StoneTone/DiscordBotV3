8977853827efffed94a5c2e47b80b33434cd1ce0
Antonio-PC <thetony3535@gmail.com>
2024-11-05 23:07:19 -0600

    update: added custom endpoint for lofi web scraper

---
+++ src/main/java/com/bot/discordbotv3/service/LofiService.java
@@ -0,0 +1,47 @@
+package com.bot.discordbotv3.service;
+
+import com.bot.discordbotv3.vo.LofiTrack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LofiService {
+
+    private static Logger logger = LoggerFactory.getLogger(LofiService.class);
+    private static RestTemplate restTemplate;
+
+    @Autowired
+    public LofiService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public static List<LofiTrack> getLofiTracks(){
+        String endpoint = "http://lofiscrap:8080/v1/lofi";
+
+        try{
+            ResponseEntity<List<LofiTrack>> response = restTemplate.exchange(
+                    endpoint,
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<LofiTrack>>() {});
+
+            if(response.getStatusCode() == HttpStatus.OK && response.getBody() != null){
+                return response.getBody();
+            }
+        }catch(RestClientException e){
+            logger.error("Failed to fetch lofi tracks: " + e.getMessage(), e);
+        }
+        return new ArrayList<>();
+    }
+}
